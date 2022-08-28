e8b611a189ee4ced942c7f17e0d3c367684d827e
Marius Corneliu Bujor <bujormarius1982@gmail.com>
2022-08-28 18:33:50 +0000

    Fixing wish.html template and code in wish app.

--- wish/models.py
+++ wish/models.py
@@ -1,10 +1,9 @@
 from django.db import models
-from django.contrib.auth.models import User
+
 
 class Wish(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    body = models.CharField(max_length=100)
+    item = models.CharField(max_length=200)
+    completed = models.BooleanField(default=False)
 
     def __str__(self):
-        return self.body
-    
+        return self.item + ' | ' + str(self.completed)
--- wish/views.py
+++ wish/views.py
@@ -1,8 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Wish
+from .forms import WishForm
+from django.contrib import messages
 
 
 def create_wish(request):
-    # print('here')
-    return render(request, "wish/wish.html", {})
+    if request.method == 'POST':
+        form = WishForm(request.POST or None)
 
-  
+        if form.is_valid():
+            form.save()
+            all_items = Wish.objects.all()
+            messages.success(request, ("Your location that you want to visit has been added to List !"))
+            return render(request, "wish/wish.html", {'all_items': all_items})
+
+    else:
+        all_items = Wish.objects.all()
+        return render(request, "wish/wish.html", {'all_items': all_items})
+
+
+def delete(request, wish_id):
+    item = Wish.objects.get(pk=wish_id)
+    item.delete()
+    messages.success(request, ('Location Has Been Deleted !'))
+    return redirect('wish_create')
+
+
+def cross_off(request, wish_id):
+    item = Wish.objects.get(pk=wish_id)
+    item.completed = True
+    item.save()
+    return redirect('wish_create')
+
+
+def uncross(request, wish_id):
+    item = Wish.objects.get(pk=wish_id)
+    item.completed = False
+    item.save()
+    return redirect('wish_create')
